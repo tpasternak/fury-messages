031c3ed110b9328cf6e4409f0911290bcd984e68
Tomasz Pasternak <tpasternak@virtuslab.com>
2019-06-12 16:29:48 +0200

    Exclamation mark

--- src/messages/hello/Constants.scala
+++ src/messages/hello/Constants.scala
@@ -2,7 +2,7 @@ package messages.hello
 
 object Constants {
   def helloMsgs = Map(
-    "pl" -> "Witaj, świecie",
-    "en" -> "Hello, world"
+    "pl" -> "Witaj, świecie!",
+    "en" -> "Hello, world!"
   )
 }
